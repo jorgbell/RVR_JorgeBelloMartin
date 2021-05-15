e3889f2057291cd6286e9db76a91475296822143
Jorge <jorgbell@ucm.es>
2021-05-15 19:09:24 +0200

    servidor del ejercicio 2

--- practica2.1/ejercicio2.cc
+++ practica2.1/ejercicio2.cc
@@ -3,10 +3,10 @@
 
 #include <iostream>
 #include <string.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
-
 /*MAN
 int bind(int sockfd, const struct sockaddr *addr,
                 socklen_t addrlen);
@@ -26,8 +26,12 @@ ssize_t sendto(int sockfd, const void *buf, size_t len, int flags,
 ssize_t recvfrom(int sockfd, void *buf, size_t len, int flags,
                         struct sockaddr *src_addr, socklen_t *addrlen);
 */
+//---------------------------------------------------------------------------------//
+//                              SERVIDOR UDP
+//---------------------------------------------------------------------------------//
 int main(int argc, char**argv){
 
+    //RECEPCION DE IP Y PUERTO
     struct addrinfo hints;
     struct addrinfo * res;
 
@@ -38,13 +42,53 @@ int main(int argc, char**argv){
     int returnCode = getaddrinfo(argv[1], argv[2],  //node, service
                        &hints, &res);               //hints, resultado
 
-    //Manejo de errores:
-    if (returnCode !=0){
+    
+    if (returnCode !=0){ //Manejo de errores:
         std::cerr << "getaddrinfo: " << gai_strerror(returnCode) << std::endl; 
         return -1;
     }
 
-    int socket(res-)
+    //------------------
+    //APERTURA DEL SOCKET
+    int sck = socket(res->ai_family, res->ai_socktype, 0);        
+   
+    if(sck == -1){  //Manejo de errores:
+        std::cerr << "ERROR en socket() : creación del socket" << std::endl; 
+        return -1;
+    }  
+
+    //------------------    
+    //ASOCIAR DIRECCIÓN AL SOCKET  
+    int bnd = bind(sck, res->ai_addr, res->addrlen);     
+
+    //------------------ 
+    //COMUNICACIÓN
+    while (true){                                                   
+        int len = 80;                                          
+        char buffer[len];        //buffer de 80 posiciones de memoria
+        struct sockaddr client;  //al ser un parametro de salida no hace falta inicializarlo
+        socklen_t clientLen = sizeof(struct sockaddr); //al ser un parametro de e/s hay que inicializarlo
+
+        int bytes = recvfrom(sck, (void * ) buffer, len, 0 , &client, &clientLen); 
+        if(bytes == -1){
+            return -1;
+        }
+        print(&client, &clientLen);
+        std::cout << "\tData: "<< buffer << std::endl;
+
+        sendto(sck, buffer, bytes, 0, &client, clientLen);        
+    }
+    close(sck);
     freeaddrinfo(res); //liberar memoria
     return 0;
+}
+
+void print(const struct sockaddr *addr, socklen_t addrlen){
+    char host[NI_MAXHOST]; //contenedores de la información sacada por getnameinfo
+    char serv[NI_MAXSERV];
+    int nameInfo = getnameinfo(addr, addrlen , 
+                                host, NI_MAXHOST,
+                                serv, NI_MAXSERV, NI_NUMERICHOST+NI_NUMERICSERV);
+        
+    std::cout << "Host: "<< host << " Port: " << serv << std::endl;
 }
